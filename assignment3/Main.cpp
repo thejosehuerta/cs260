f5791e5f51060a0331ecc0d7fe931735ff9bbe7c
thejosehuerta <jlhv7896@gmail.com>
2021-01-22 00:03:46 -0800

    Final update. Program finished and working

--- assignment3/Main.cpp
+++ assignment3/Main.cpp
@@ -21,8 +21,9 @@ bool isNumeric(string input) {
     return true;
 }
 
-
 int main(int argc, char **argv) {
+    Queue *my_queue = new Queue();
+
     bool running = true;
     cout << endl << "----------------------------------" << endl;
     cout << "Welcome to your linked-list queue.";
@@ -30,33 +31,52 @@ int main(int argc, char **argv) {
     while(running) {
         cout << endl << "----------------------------------";
         cout << endl << "What do you want to do?" << endl;
-        cout << endl << "1. Enqueue an element";
-        cout << endl << "2. Dequeue front element";
-        cout << endl << "3. View front element";
-        cout << endl << "4. Print all elements";
+        cout << endl << "1. Enqueue an number";
+        cout << endl << "2. Dequeue number in front";
+        cout << endl << "3. View number in front of queue";
+        cout << endl << "4. Print all numbers in queue";
         cout << endl << "5. Exit" << endl;
 
         string input;
+        string enq_input;
+
         cout << "Enter a number: ";
         cin >> input;
         //if loop checks for numeric input
         if (isNumeric(input)) {
+            //if input is numeric, stoi turns it into an int and passes it to num_input
             int num_input = stoi(input);
             switch (num_input) {
             case 1:
-                cout << "1" << endl;
+                cout << "Enter a number to enqueue: ";
+                cin >> enq_input;
+                if(isNumeric(enq_input)) {
+                     //if enq_input is numeric, stoi turns it into an int and passes it to enq_int
+                    int enq_int = stoi(enq_input);
+                    my_queue->Enqueue(enq_int);
+                    cout << endl << enq_int << " successfully enqueued!";
+                } else {
+                    //if enq_input is non-numeric
+                    cout << endl << "Not a number.";
+                }
                 break;
 
             case 2:
-                cout << "2" << endl;
+                if(my_queue->isEmpty()) {
+                    my_queue->Dequeue();
+                } else {
+                    my_queue->Dequeue();
+                    cout << endl << "Number in front successfully dequeued." << endl;
+                }
                 break;
 
             case 3:
-                cout << "3" << endl;
+                cout << endl << "Number in front of queue: " << my_queue->Front() << endl;
                 break;
 
             case 4:
-                cout << "4" << endl;
+                cout << endl << "Printing queue:" << endl;
+                my_queue->Print();
                 break;
 
             case 5:
@@ -65,45 +85,13 @@ int main(int argc, char **argv) {
                 break;
             
             default:
-                cout << "Out of range." << endl;
+                cout << endl << "Out of range." << endl;
                 break;
             }
         //if inout is non-numeric, it returns this statement
         } else {
             cout << endl << "'" << input << "'" << " is not a number." << endl;
         }
-
-    
-
-
-
-
-
     }
-
-   
-
-
-    //Create a new queue named my_queue
-    Queue *my_queue = new Queue();
-    //Call front() function to return the first element
-    //cout << "my_queue->Front: " << my_queue->Front() << endl;
-    //my_queue->Print();
-    //my_queue->Dequeue();
-
-    //my_queue->Enqueue(3);
-    //my_queue->Enqueue(6);
-
-    //cout << "my_queue->Front: " << my_queue->Front() << endl;
-   
-
-    //my_queue->Dequeue();
-
-    //cout << "my_queue->Front: " << my_queue->Front() << endl;
-    //my_queue->Print();
-
-
-
-
     return 0;
 }
--- assignment3/Queue.cpp
+++ assignment3/Queue.cpp
@@ -25,13 +25,14 @@ bool Queue::isEmpty() {
     }
 }
 
+//Enqueues int value
 void Queue::Enqueue(int new_value) {
     //Create a new node
     Node *temp = new Node();
     //Storing "new_value" to new node's "data" value
     temp->data = new_value;
     //New node will point to null 
-    temp->next = 0;
+    temp->next = NULL;
     //Calls isEmpty() function to check if queue is empty
     if(isEmpty()) {
         front = rear = temp;
@@ -45,36 +46,43 @@ void Queue::Enqueue(int new_value) {
 void Queue::Dequeue() {
     Node *temp = front;
     if(isEmpty()) {
-        cout << "Queue is empty! Nothing to dequeue." << endl;
+        cout << endl << "Queue is empty! Nothing to dequeue." << endl;
         return;
     }
     if(front == rear) {
         front = rear = NULL;
     } else {
+        //the front pointer will point to the next element in the queue
         front = front->next;
     }
+    //because front is now pointing to the second element, the original "first" element will be deleted and memory will be freed
     free(temp);
 }
 
+//Returns the value belonging to the front node
 int Queue::Front() {
     if(isEmpty()) {
-        cout << "Queue is empty! No element in front." << endl;
+        cout << endl << "Queue is empty! No element in front." << endl;
         return -1;
     } else {
         return front->data;
     }
 }
 
+//Prints all elements in queue
 void Queue::Print() {
     if(isEmpty()) {
-        cout << "Queue is empty! Nothing to print." << endl;
+        cout << endl << "Queue is empty! Nothing to print." << endl;
         return;
     } else {
         Node *temp = front;
+        cout << "Front\t";
+        //prints elements until temp == NULL (when the last node is reached)
         while(temp != NULL) {
-            cout << temp->data << endl;
+            cout << temp->data << "\t";
             temp = temp->next;
         }
+        cout << "Rear";
         return;
     }
 }
